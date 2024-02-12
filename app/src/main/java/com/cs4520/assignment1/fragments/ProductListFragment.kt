fdd966ebf55d7453dc16a0cf4951787bdb548977
Nicholas Tietje <tietje.n@northeastern.edu>
2024-02-12 07:54:03 -0500

    Created ProductListFragment

--- app/src/main/java/com/cs4520/assignment1/fragments/ProductListFragment.kt
+++ app/src/main/java/com/cs4520/assignment1/fragments/ProductListFragment.kt
@@ -1,7 +1,26 @@
 package com.cs4520.assignment1.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.cs4520.assignment1.Product
+import com.cs4520.assignment1.R
+import com.cs4520.assignment1.productsDataset
 
 class ProductListFragment: Fragment() {
-
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.product_view, container, false)
+        val products = productsDataset.map { Product(it) }
+        val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        return view
+    }
 }

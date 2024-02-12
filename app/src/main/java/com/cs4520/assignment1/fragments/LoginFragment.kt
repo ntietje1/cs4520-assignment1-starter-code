ea8a9f0cac121470efd083a26fadc4e06516487f
Nicholas Tietje <tietje.n@northeastern.edu>
2024-02-12 07:55:31 -0500

    Created LoginFragment with login button

--- app/src/main/java/com/cs4520/assignment1/fragments/LoginFragment.kt
+++ app/src/main/java/com/cs4520/assignment1/fragments/LoginFragment.kt
@@ -1,7 +1,24 @@
 package com.cs4520.assignment1.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.cs4520.assignment1.R
 
 class LoginFragment: Fragment() {
-
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.login_view, container, false)
+        val loginButton = view.findViewById<View>(R.id.loginButton)
+        val navController = findNavController()
+        loginButton.setOnClickListener {
+            navController.navigate(R.id.action_loginFragment_to_productListFragment)
+        }
+        return view
+    }
 }

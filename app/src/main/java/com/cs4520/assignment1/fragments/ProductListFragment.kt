3849f5f7cb6a165ad1ced5c838500c0386b5ebf0
Nicholas Tietje <tietje.n@northeastern.edu>
2024-03-12 12:05:24 -0400

    updated product fragment to asynchronously call api or db

--- app/src/main/java/com/cs4520/assignment1/fragments/ProductListFragment.kt
+++ app/src/main/java/com/cs4520/assignment1/fragments/ProductListFragment.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.cs4520.assignment1.Product
-import com.cs4520.assignment1.ProductAdapter
+import androidx.room.Room
+import com.cs4520.assignment1.data.Product
 import com.cs4520.assignment1.R
-import com.cs4520.assignment1.productsDataset
+import com.cs4520.assignment1.data.db.ProductDatabase
+import com.cs4520.assignment1.data.http.RetrofitInstance
+import com.cs4520.assignment1.views.ProductAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ProductListFragment: Fragment() {
     override fun onCreateView(
@@ -19,10 +30,57 @@ class ProductListFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.product_view, container, false)
-        val products = productsDataset.map { Product(it) }
+        val progressBar: ProgressBar = view.findViewById(R.id.progressBar)
+        val noProducts: View = view.findViewById(R.id.noProducts)
+
         val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(context)
-        recyclerView.adapter = ProductAdapter(products)
+
+        progressBar.isVisible = true
+
+        val db = Room.databaseBuilder(
+            requireContext(),
+            ProductDatabase::class.java, "database-name"
+        ).build()
+
+        val productDao = db.productDao()
+        var products: List<Product> = listOf()
+
+        RetrofitInstance.api.getProducts(3).enqueue(object : Callback<List<Product>> {
+            override fun onResponse(call: Call<List<Product>>, response: Response<List<Product>>) {
+                progressBar.isVisible = false
+                if (response.isSuccessful) {
+                    products = response.body()!!
+                    products = products.filter { it.type != null && it.price != null}
+                    products = products.distinctBy { it.name }
+
+                    viewLifecycleOwner.lifecycleScope.launch {
+                        withContext(Dispatchers.IO) {
+                            productDao.deleteAll()
+                            productDao.insertAll(products)
+                        }
+                    }
+
+                    recyclerView.adapter = ProductAdapter(products)
+                } else {
+                    println("Error: ${response.code()}")
+                }
+                noProducts.isVisible = products.isEmpty()
+            }
+
+            override fun onFailure(call: Call<List<Product>>, t: Throwable) {
+                viewLifecycleOwner.lifecycleScope.launch {
+                    withContext(Dispatchers.IO) {
+                        products = productDao.get()
+                    }
+                    withContext(Dispatchers.Main) {
+                        progressBar.isVisible = false
+                        recyclerView.adapter = ProductAdapter(products)
+                        noProducts.isVisible = products.isEmpty()
+                    }
+                }
+            }
+        })
         return view
     }
 }
